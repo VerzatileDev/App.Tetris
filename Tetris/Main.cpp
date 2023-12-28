9c1b493fa23f84b306d258b9a1b942f4c8d3e5e0
VerzatileDev <47317248+VerzatileDev@users.noreply.github.com>
2023-12-28 17:37:46 +0200

    [ADD] Window Loop
    Establish a core loop for an SFML window
    
    While window is open,
    Make an event,
    While polling for events
    look for if an event such as Closed has been sent
    if sent then Close the window.
    
    Once Out of the loop clear window to black before rendering
    and then display it and start again from the start of the loop of isopen

--- Tetris/Main.cpp
+++ Tetris/Main.cpp
@@ -3,5 +3,21 @@
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode::VideoMode(320, 480), "Tetris");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// Close window: exit
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear(sf::Color::Black);
+
+
+		window.display();
+	}
 	return 0;
 }
